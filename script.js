534459b87cc0e37a26e29652a52bd8d11bc78e2a
Cjgaleon <124644819+Cjgaleon@users.noreply.github.com>
2023-05-09 11:38:50 -0400

    Calculator

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
-const insert = () => {
-    let type = document.querySelector("#type").value
-    let label = document.querySelector("#label").value
-    let placeholder = document.querySelector("#placeholder").value
-  
-    switch (type) {
-      case 'text':
-        appendText(label, placeholder)
-        break
-      case 'radio':
-        appendRadio(label, placeholder)
-        break
-      case 'button':
-        appendButton(label)
-        break
+let id = 0;
+const submit = () => {
+    let type = document.querySelector('#type').value;
+    let label = document.querySelector('#label').value;
+    let placeholder = document.querySelector('#placeholder').value;
+    switch(type){
+        case 'text' : appendText(label, placeholder);
+            break
+        case 'Radio': appendRadio(label, placeholder);
+            break
+        case 'button': appendButton(label, placeholder);
     }
-  }
-  
-  const appendText = (label = "No label", placeholder = "No placeholder") => {
-    let el = document.createElement('input')
-    el.setAttribute('type', 'text')
-    el.setAttribute('class', 'form-control mt-1')
-    el.setAttribute('placeholder', placeholder)
-    el.setAttribute('aria-label', label)
-    document.querySelector("#elements").append(el)
-  }
-  
-  const appendRadio = (label = "No label", placeholder = "No placeholder") => {
-    let el = document.createElement('input');
-    el.setAttribute('type', 'radio');
-    el.setAttribute('name', 'radio-group');
-  
-    let labelEl = document.createElement('label');
-    labelEl.innerHTML = label;
-  
+}
+
+const appendText = (label = 'no Label', placeholder = 'no placeholder') => {
+    id += 1;
+    const PID = id; 
     let container = document.createElement('div');
-    container.setAttribute('class', 'form-check');
-  
-    container.appendChild(el);
-    container.appendChild(labelEl);
-  
-    document.querySelector("#elements").appendChild(container);
-  };
-  
-  const appendButton = (label = "No label") => {
-    let el = document.createElement('button')
-    el.setAttribute('type', 'button')
-    el.setAttribute('class', 'btn btn-primary mt-1')
-    el.innerText = label
-    document.querySelector("#elements").append(el)
-  }
+    let labelT = document.createElement('p');
+    let del = document.createElement('button');
+    del.innerHTML = "del";
+    del.setAttribute('onclick', 'del('+ PID +')');
+    labelT.innerHTML = label;
+    container.setAttribute('id', id);
+    let el = document.createElement('input');
+    el.setAttribute('placeholder', placeholder);
+    container.append(labelT)
+    container.append(el)
+    container.append(del)
+    document.querySelector('#results').append(container);
+
+}
+
+const appendRadio = (label ='no label', placeholder ='no placeholder') => {
+    id += 1;
+    const PID = id; 
+    let container = document.createElement('div');
+    let labelT = document.createElement('p');
+    let del = document.createElement('button');
+    del.innerHTML = "del";
+    del.setAttribute('onclick', 'del('+ PID +')');
+    labelT.innerHTML = label;
+    container.setAttribute('id', id);
+    let el = document.createElement('input');
+    el.setAttribute('type', 'radio')
+    el.setAttribute('placeholder', placeholder);
+    container.append(labelT)
+    container.append(el)
+    container.append(del)
+    document.querySelector('#results').append(container);
+}
+
+const del = (del) =>{
+    document.getElementById(del).remove(this);
+}
